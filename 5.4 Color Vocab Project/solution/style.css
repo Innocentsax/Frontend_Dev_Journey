bdf7a8affc91a1c275e703a52f80ccb1b449dd3a
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-21 22:06:32 +0100

    CSS color selector project

---
+++ 5.4 Color Vocab Project/solution/style.css
@@ -0,0 +1,28 @@
+img {
+  height: 200px;
+  width: 200px;
+}
+
+.color-title {
+  font-weight: normal;
+}
+
+#red {
+  color: red;
+}
+
+#blue {
+  color: blue;
+}
+
+#orange {
+  color: orange;
+}
+
+#green {
+  color: green;
+}
+
+#yellow {
+  color: yellow;
+}
