cf85601f5bde1e162e44f09f004d04d0c35b3e1a
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-09-14 10:20:30 +0100

    Update ReactTask.jsx

--- 8.1 CSS Float/ReactTask.jsx
+++ 8.1 CSS Float/ReactTask.jsx
@@ -30,3 +30,36 @@ className="counter-button"
 );
 };
 ``` */
+
+import React, { useState } from "react";
+
+export default function Clicker() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    if (clickCount >= 3) {
+      // Reset the count to 0 if it exceeds 3
+      setClickCount(0);
+    } else {
+      // Increment the click count
+      setClickCount(clickCount + 1);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="counter-button"
+        >
+          Click Me
+        </button>
+      </div>
+      <div>
+        <p>Click Count: {clickCount}</p>
+      </div>
+    </React.Fragment>
+  );
+}
