2fb3b68ff6a39daf2774c5a12d5fe53df71c0713
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-09-14 10:19:16 +0100

    Create ReactTask.jsx

---
+++ 8.1 CSS Float/ReactTask.jsx
@@ -0,0 +1,32 @@
+/*Question 1
+====> <=====
+Complete the React application below (25 points)
+Implement logic to track clicks that increments when the <button> element it is clicked.
+Populate the
+tag with the current click count
+
+if the click count is > 3 return the count to 0.
+```
+import React, { useState } from "react";
+export default function Clicker() {
+
+function handleClick() {
+//implement handleClick function
+}
+
+return (
+<React.Fragment>
+{/use state change here/}
+
+
+<form>
+<button
+type="button"
+onClick={handleClick}
+className="counter-button"
+    >Click me</button>
+  </form>
+</React.Fragment>
+);
+};
+``` */
