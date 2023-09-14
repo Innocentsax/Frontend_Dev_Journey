9c38bd3fdfe9d38413faddb8d958bed0c706d869
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-09-14 10:23:12 +0100

    Update ReactTask.jsx

--- 8.1 CSS Float/ReactTask.jsx
+++ 8.1 CSS Float/ReactTask.jsx
@@ -63,3 +63,71 @@ export default function Clicker() {
     </React.Fragment>
   );
 }
+
+=====> Question 2 <====
+/*
+
+What is the Virtual DOM? (10 points)
+
+How does React determine when to update a component, explain? (15 points)
+
+Virtual DOM is a concept in React that acts as an intermediary layer between 
+your JavaScript code and the actual browser DOM. 
+
+React determines when to update a component by comparing the Virtual DOM with 
+the previous version. It updates the real DOM only when it identifies differences due 
+to state, props, or other triggers, making updates efficient and responsive.
+3.
+A React Hook allows us to encapsulate logic into a separate hook(function) so that it can be reusued in different components.
+
+Create a custom hook called useTimer that returns the seconds since the hook was rendered. (25 points)
+
+Hint: Leverage setInterval or setTimeout in useEffect for this.
+
+Imagine that your hook would be used as shown in the component below.
+
+function TimerInMinutes() {
+  const seconds = useTimer();
+
+  const minutes = Math.trunc(seconds / 60);
+
+  return (
+    <>
+      <p>Minutes: {minutes}</p>
+      <p>time: {seconds}</p>
+    </>
+  )
+}
+*/
+import { useState, useEffect } from 'react';
+
+function useTimer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000); 
+
+    return () => {
+      clearInterval(timer); 
+    };
+  }, []);
+
+  return seconds;
+}
+
+// Component TimerInMinutes
+function TimerInMinutes() {
+  const seconds = useTimer();
+  const minutes = Math.trunc(seconds / 60);
+
+  return (
+    <div>
+      <p>Minutes: {minutes}</p>
+      <p>Time: {seconds}</p>
+    </div>
+  );
+}
+
+export default TimerInMinutes;
